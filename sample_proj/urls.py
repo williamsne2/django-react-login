261bb266fb7862b7107be66f08e7f1dd4741e4b9
Nick <williamsne2@gmail.com>
2019-07-18 13:29:24 -0500

    login, signup, signin implemented

--- sample_proj/urls.py
+++ sample_proj/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    
 ]
